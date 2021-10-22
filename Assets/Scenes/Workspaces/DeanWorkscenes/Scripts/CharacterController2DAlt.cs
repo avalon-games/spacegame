bda2e36e1a656ae97dcdfcf992766ab6ef0da8a8
nekopudding <61680928+nekopudding@users.noreply.github.com>
2021-10-22 15:05:25 -0700

    moved folders, renamed controller script

--- Assets/Scenes/Workspaces/DeanWorkscenes/Scripts/CharacterController2DAlt.cs
+++ Assets/Scenes/Workspaces/DeanWorkscenes/Scripts/CharacterController2DAlt.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController2D : MonoBehaviour
+public class CharacterController2DAlt : MonoBehaviour
 {
     
     Rigidbody2D rb;
