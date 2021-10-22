5198fd39d08c0619898e5291c3d606e9151c1cea
nekopudding <61680928+nekopudding@users.noreply.github.com>
2021-10-22 15:29:28 -0700

    basic movement, camera, parallax implemented (#4)
    * basic movement, parallax, camera setup
    
    * moved script, prefab folders, renamed controller script

---
+++ Assets/Scenes/Workspaces/DeanWorkscenes/Scripts/CharacterController2DAlt.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2DAlt : MonoBehaviour
+{
+    
+    Rigidbody2D rb;
+    Collider2D coll;
+    LayerMask groundLayer;
+    //LayerMask enemyLayer;
+    //Animator animator;
+    enum State {idle, running, jumping, falling, hurt}; //animation states, decides interactions
+    State state;
+    [SerializeField] int playerSpeed = 5;
+    [SerializeField] float jumpForce = 300f;
+    [SerializeField] float airControl = 0.8f;
+    //[SerializeField] float hurtForce = 2f;
+    [SerializeField] float groundDetectRadius = 0.24f;
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .2f;
+    Vector3 m_Velocity = Vector3.zero;
+
+    float horizontalInput;
+    bool jumpButton;
+    bool isOnGround;
+
+    void Start() { 
+        state = State.idle;
+        rb = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
+        coll = GetComponent<CapsuleCollider2D>();
+        groundLayer = LayerMask.GetMask("Ground");
+        //enemyLayer = LayerMask.GetMask("Enemy");
+    }
+
+    void Update() {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump")) jumpButton = true;
+        RaycastHit2D groundHit = Physics2D.Raycast(coll.bounds.center, Vector2.down, coll.bounds.extents.y + groundDetectRadius, groundLayer);
+        Debug.DrawRay(coll.bounds.center, Vector2.down * (coll.bounds.extents.y + groundDetectRadius)); 
+        isOnGround = groundHit.collider != null;
+
+        AssignState();
+        //animator.SetInteger("state", (int)state);
+
+        
+    }
+
+    void FixedUpdate() {
+        MovePlayer();
+    }
+
+    void MovePlayer() {
+        Vector3 targetVelocity;
+        if(state != State.hurt) {
+            if (isOnGround) {
+                if (horizontalInput < 0)
+                    targetVelocity = new Vector2(-playerSpeed, rb.velocity.y);
+                else if (horizontalInput > 0)
+                    targetVelocity = new Vector2(playerSpeed, rb.velocity.y);
+                else
+                    targetVelocity = new Vector2(0, rb.velocity.y);
+                if (jumpButton) {
+                    rb.AddForce(new Vector2(0f, jumpForce));
+                    isOnGround = false;
+                    jumpButton = false;
+                }
+            }
+            else {
+                if (horizontalInput < 0)
+                    targetVelocity = new Vector2(-playerSpeed*airControl, rb.velocity.y);
+                else if (horizontalInput > 0)
+                    targetVelocity = new Vector2(playerSpeed*airControl, rb.velocity.y);
+                else
+                    targetVelocity = new Vector2(0, rb.velocity.y);
+            }
+            if (horizontalInput < 0)
+                transform.localScale = new Vector2(-1, 1);
+            else if (horizontalInput > 0)
+                transform.localScale = new Vector2(1, 1);
+
+			rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+        }
+    }
+    void AssignState() {
+        //if (hurt)
+        //    state = State.hurt;
+        if (rb.velocity.y > 0)
+            state = State.jumping;
+        else if (rb.velocity.y < 0)
+            state = State.falling;
+        else if (Mathf.Abs(horizontalInput) > 0)
+            state = State.running;
+        else
+            state = State.idle;
+    }
+}
---
+++ Assets/Scenes/Workspaces/DeanWorkscenes/Scripts/Parallax.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    float length, startpos;
+    [SerializeField] GameObject cam;
+    [SerializeField] float parallaxEffect;
+    Vector3 v = Vector3.zero;
+    [SerializeField] float smoothing = 5f;
+
+    void Start() {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update() {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        //transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        Vector3 targetPosition = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref v, smoothing);
+
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
+    }
+}
