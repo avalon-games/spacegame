f833f5d22b39b18ec191df7bdb8d4bea5c99edad
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-02-04 11:44:59 -0800

    integrated new grappling hook

--- Assets/Scripts/PlayerControlScripts/GrapplingHook.cs
+++ Assets/Scripts/PlayerControlScripts/GrapplingHook.cs
@@ -17,15 +17,22 @@ public class GrapplingHook : MonoBehaviour
     Rigidbody2D rb;
     public Transform gunPoint;
     LineRenderer rope;
+    Transform anchorPoint;
+    public Sprite attachedSprite;
+    Sprite detachedSprite;
+    SpriteRenderer sp;
 
     [Range(0, 30)] public float maxRange = 10;
-    
+
+    Vector2 launchDirection;
     public float launchSpeed = 100f;
 
     void Awake() {
         rb = GetComponent<Rigidbody2D>();
         cam = Camera.main;
         rope = GetComponent<LineRenderer>();
+        sp = GetComponent<SpriteRenderer>();
+        detachedSprite = sp.sprite;
         this.gameObject.SetActive(false);
     }
 
@@ -33,29 +40,83 @@ public class GrapplingHook : MonoBehaviour
     {
         isAttached = false;
         transform.position = gunPoint.position;
-        Vector2 distanceVector = (cam.ScreenToWorldPoint(Input.mousePosition) - transform.position);
-        distanceVector.Normalize();
-        rb.velocity = distanceVector * launchSpeed;
+        launchDirection = (cam.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+        launchDirection.Normalize();
+        rb.velocity = launchDirection * launchSpeed;
+
+        GetHitObject(launchDirection);
+        if (anchorPoint != null) {
+            float timeToHit = Vector2.Distance(gunPoint.position, anchorPoint.position)/launchSpeed;
+            StartCoroutine(AttachAfterTime(timeToHit));
+        }  
     }
 
 	private void LateUpdate() {
-        if (!isAttached && (Vector2.Distance(transform.position, gunPoint.position) > maxRange)) {
-            grappleRelease = true;
-        }
-        rope.SetPosition(0, gunPoint.position);
-        rope.SetPosition(1, transform.position);
+		if (!isAttached && (Vector2.Distance(transform.position, gunPoint.position) > maxRange)) {
+			grappleRelease = true;
+			ClearAnchorPoint();
+		}
+		SetRopeEndpoints();
+		SetRotation();
+        SetSprite();
+	}
+
+	private void SetRopeEndpoints() {
+		rope.SetPosition(0, gunPoint.position);
+		rope.SetPosition(1, transform.position);
+	}
+
+	private void SetRotation() {
+		if (anchorPoint) {
+			transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(anchorPoint.position.y - gunPoint.position.y, anchorPoint.position.x - gunPoint.position.x) * 180 / Mathf.PI - 45);
+		} else {
+			transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(launchDirection.y, launchDirection.x) * 180 / Mathf.PI - 45);
+		}
+	}
+    void SetSprite() {
+		if (isAttached) {
+            sp.sprite = attachedSprite;
+		}
+        else {
+            sp.sprite = detachedSprite;
+		}
+	}
+
+	void GetHitObject(Vector2 direction) {
+        RaycastHit2D[] hits = Physics2D.RaycastAll(gunPoint.position, direction, maxRange);
+
+        foreach(RaycastHit2D hit in hits) {
+            if(hit.collider.CompareTag("Grappable")) {
+                anchorPoint = hit.collider.transform;
+                break;
+			}
+		}
+     }
+    public void ClearAnchorPoint() {
+        anchorPoint = null;
     }
 
-	private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.gameObject.CompareTag("Grappable")) {
-            //snap position to centre of tile, each grid is 0.99 in size, /0.99 gets actual grid cell number, *0.99 translates it back to world coordinate
-            transform.position = new Vector2(Mathf.Floor(transform.position.x / 0.99f) * 0.99f + 0.495f,
-                                            Mathf.Floor(transform.position.y / 0.99f) * 0.99f + 0.495f);
-            rb.velocity = Vector2.zero;
-           
-            isAttached = true;
-        }
+    void Attach() {
+        transform.position = anchorPoint.position;
+        rb.velocity = Vector2.zero;
+
+        isAttached = true;
     }
+    IEnumerator AttachAfterTime(float time) {
+        yield return new WaitForSeconds(time);
+        if (anchorPoint != null) Attach();
+	}
+
+    //private void OnTriggerEnter2D(Collider2D collision) {
+    //    if (collision.gameObject.CompareTag("Grappable")) {
+    //        //snap position to centre of tile, each grid is 0.99 in size, /0.99 gets actual grid cell number, *0.99 translates it back to world coordinate
+    //        transform.position = new Vector2(Mathf.Floor(transform.position.x / 0.99f) * 0.99f + 0.495f,
+    //                                        Mathf.Floor(transform.position.y / 0.99f) * 0.99f + 0.495f);
+    //        rb.velocity = Vector2.zero;
+           
+    //        isAttached = true;
+    //    }
+    //}
 
 	private void FixedUpdate() {
         if (grappleRelease) {
@@ -65,15 +126,20 @@ public class GrapplingHook : MonoBehaviour
 
 	void ReturnHook () {
         isAttached = false;
-        Vector2 distanceVector = (gunPoint.position - transform.position).normalized;
+        Vector2 distanceVector = (gunPoint.position - transform.position);
+        distanceVector.Normalize();
         rb.velocity = distanceVector * launchSpeed;
 
-        if (Vector2.Distance(transform.position,gunPoint.position) <= 3f) {
+        if (Vector2.Distance(transform.position,gunPoint.position) <= 1f) {
             transform.position = gunPoint.position;
             rb.velocity = Vector2.zero;
             grappleRelease = false;
             this.gameObject.SetActive(false);
         }
     }
+
+	private void OnDisable() {
+        ClearAnchorPoint();
+	}
 }
 //add rendering of the grappling rope
